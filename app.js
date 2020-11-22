2294ad8bde8dfd5f6b3d89da1d275a07a80894f7
Mathew B <bogaert@email.arizona.edu>
2020-11-22 12:05:11 -0700

    finished site

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 
-const fs = require('fs');
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 const generatePage = require('./src/page-template');
 
 const promptUser = () => {
@@ -139,38 +139,20 @@ Add a New Project
 
 
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  console.log(portfolioData);
-  
-  const pageHTML = generatePage(portfolioData);
-   fs.writeFile('./dist/index.html', pageHTML, err => {
-    if (err) {
-        console.log(err);
-        return;
-      }
-     console.log('Page created! Check out index.html in this directory to see it!');
-
-     fs.copyFile("./src/style.css", "./dist/style.css", err => {
-         if (err) {
-             console.log(err);
-             return;
-         }
-         console.log("Style sheet copied successfully!");
-     });
-
-   });
-
-});
-   
-
-
-
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
